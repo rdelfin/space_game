014ca9564094e7fab03f37845a7458d900f8e0e0
Ricardo Delfin <me@rdelfin.com>
2020-04-15 23:10:54 +0100

    Added basic ggez template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use ggez::event::{self, EventHandler};
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+
 fn main() {
-    println!("Hello, world!");
+    let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Ricardo Delfin")
+        .build()
+        .expect("Could not create ggez context");
+
+    let mut my_game = MyGame::new(&mut ctx);
+
+    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occurred: {}", e),
+    }
+}
+
+struct MyGame {}
+
+impl MyGame {
+    pub fn new(_ctx: &mut Context) -> MyGame {
+        MyGame {}
+    }
+}
+
+impl EventHandler for MyGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+
+        graphics::present(ctx)
+    }
 }
