f3c24b9dc44f720a315efa40f97b23bf3026f834
Ricardo Delfin <me@rdelfin.com>
2020-05-03 23:46:37 +0100

    Added directional controlls for user, with inertia

--- src/systems/controller.rs
+++ src/systems/controller.rs
@@ -2,22 +2,23 @@ use crate::components::{RigidBody, UserControlled};
 use crate::resources::KeyboardState;
 
 use ggez::input::keyboard::KeyCode;
-use specs::{Read, ReadStorage, System, WriteStorage};
+use ggez::nalgebra::Rotation2;
+use specs::{Read, System, WriteStorage};
 
 pub struct ControllerSystem;
 
 impl<'a> System<'a> for ControllerSystem {
     type SystemData = (
         Read<'a, KeyboardState>,
-        ReadStorage<'a, UserControlled>,
+        WriteStorage<'a, UserControlled>,
         WriteStorage<'a, RigidBody>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (key_state, _, mut bodies) = data;
+        let (key_state, mut users, mut bodies) = data;
 
         use specs::Join;
-        for body in (&mut bodies).join() {
+        for (body, user) in (&mut bodies, &mut users).join() {
             let (left_pressed, right_pressed, up_pressed, down_pressed) = (
                 key_state.is_pressed(KeyCode::A),
                 key_state.is_pressed(KeyCode::D),
@@ -25,20 +26,27 @@ impl<'a> System<'a> for ControllerSystem {
                 key_state.is_pressed(KeyCode::S),
             );
 
-            if left_pressed == right_pressed {
-                body.vel.x = 0.0;
-            } else if left_pressed {
-                body.vel.x = -200.0;
-            } else {
-                body.vel.x = 200.0;
-            }
+            let mut target_vel = user.get_dir_vec();
 
             if up_pressed == down_pressed {
-                body.vel.y = 0.0;
-            } else if up_pressed {
-                body.vel.y = -200.0;
+                target_vel *= 0.0;
+            } else if down_pressed {
+                target_vel *= -1.0;
+            }
+
+            if left_pressed == right_pressed {
+                // Do nothing (no need to touch target vector, but this will make
+                // other statements easier)
+            } else if left_pressed {
+                target_vel = Rotation2::new(user.max_rot_speed) * target_vel;
             } else {
-                body.vel.y = 200.0;
+                target_vel = Rotation2::new(-user.max_rot_speed) * target_vel;
+            }
+
+            // Only update angle the object is moving.
+            body.vel += user.inertia * (target_vel - body.vel);
+            if body.vel.norm() > 0.0 {
+                user.angle = body.vel.y.atan2(body.vel.x)
             }
         }
     }
