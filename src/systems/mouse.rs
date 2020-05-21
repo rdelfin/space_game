c836983de84918f0daa375ac0a716bfdcc43efe0
Ricardo Delfin <me@rdelfin.com>
2020-05-21 21:13:58 +0100

    Changed it so that click will drop a building down

--- src/systems/mouse.rs
+++ src/systems/mouse.rs
@@ -2,6 +2,7 @@ use crate::components::{GridPosition, Selectable};
 use crate::resources::MouseState;
 use crate::utils::grid;
 
+use ggez::input::mouse::MouseButton;
 use specs::{Read, System, WriteStorage};
 
 pub struct TileDragSystem;
@@ -20,6 +21,10 @@ impl<'a> System<'a> for TileDragSystem {
         for (selectable, grid_position) in (&mut selectables, &mut grid_positions).join() {
             if selectable.selected {
                 grid_position.0 = grid::position_to_grid(mouse_state.position());
+
+                if mouse_state.pressed(MouseButton::Left) {
+                    selectable.selected = false;
+                }
             }
         }
     }
