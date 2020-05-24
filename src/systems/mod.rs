e9bdd3d283a92c491246e9e3fad81356a78a9304
Ricardo Delfin <me@rdelfin.com>
2020-05-24 19:09:14 +0100

    Moved mouse.rs to tile_placement.rs in systems

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,9 +1,9 @@
 mod animation;
-mod mouse;
 mod render;
+mod tile_placement;
 mod tiles;
 
 pub use self::animation::SpriteAnimation;
-pub use self::mouse::TileDragSystem;
 pub use self::render::RenderSystem;
+pub use self::tile_placement::TileDragSystem;
 pub use self::tiles::TilePositionSystem;
