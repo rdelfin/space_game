f8a001a70fce00e23722c22af13d4b02bec12732
Ricardo Delfin <me@rdelfin.com>
2020-05-24 18:47:15 +0100

    Removed unused method in component mod

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -5,10 +5,3 @@ mod sprite;
 
 pub use self::grid::{GridPosition, Selected};
 pub use self::sprite::{Position, Sprite};
-
-pub fn register_components(world: &mut World) {
-    world.register::<Position>();
-    world.register::<Sprite>();
-    world.register::<GridPosition>();
-    world.register::<Selected>();
-}
