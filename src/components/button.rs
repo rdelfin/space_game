20a4ec6751e753f4ddccebd8f228f1c8d470393c
Ricardo Delfin <me@rdelfin.com>
2020-05-25 11:15:05 +0100

    Slightly reworked button components

--- src/components/button.rs
+++ src/components/button.rs
@@ -1,5 +1,17 @@
-use specs::prelude::{Component, NullStorage};
+use ggez::graphics::Rect;
+use specs::prelude::{Component, NullStorage, VecStorage};
+
+#[derive(Component, Debug)]
+#[storage(VecStorage)]
+pub struct Pressable {
+    pub click_box: Rect,
+}
+impl Pressable {
+    pub fn new(click_box: Rect) -> Pressable {
+        Pressable { click_box }
+    }
+}
 
 #[derive(Default, Component, Debug)]
 #[storage(NullStorage)]
-pub struct Pressable;
+pub struct Pressed;
